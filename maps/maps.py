46c534345f44475de2ee7373ca1b0933a2303db3
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-02-19 13:23:31 +0300

    MapExercise completed

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,19 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+
+        def movie_is_suitable(movie: dict) -> bool:
+            return (
+                movie.get("rating_kinopoisk", 0) != 0
+                and movie.get("rating_kinopoisk", 0) != ""
+                and movie.get("rating_kinopoisk", 0) != "0"
+                and len(movie.get("country", "").split(sep=",")) > 1
+            )
+
+        ratings = list(
+            map(lambda x: float(x["rating_kinopoisk"]), filter(movie_is_suitable, list_of_movies))
+        )
+        return sum(ratings) / len(ratings)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +40,14 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+
+        def movie_is_suitable(movie: dict) -> bool:
+            rating_kinopoisk = movie.get("rating_kinopoisk", 0)
+            if rating_kinopoisk:
+                return float(rating_kinopoisk) >= rating
+            return False
+
+        filtered_and_mapped = list(
+            map(lambda x: x["name"].count("и"), filter(movie_is_suitable, list_of_movies))
+        )
+        return sum(filtered_and_mapped)
